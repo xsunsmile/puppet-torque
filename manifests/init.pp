e5fe9741cea8bb718ccb299b3fcbcab1c8abba69
Hao SUN <xsunsmile@gmail.com>
2011-05-23 15:40:32 +0900

    init

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+
+
+class torque {
+	include torque::compile
+	include torque::install
+	include torque::service
+	# add yours:
+	# include torque::extra
+}
---
+++ manifests/install.pp
@@ -0,0 +1,4 @@
+
+class torque::install {
+
+}
