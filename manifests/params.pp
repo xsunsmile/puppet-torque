9ad4aa6995d3a130a44c4785e896b8e9fe9e55e7
Hao SUN <xsunsmile@gmail.com>
2011-05-25 17:45:38 +0900

    tiny fix

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,11 +2,12 @@
 class torque::params {
 
 	$install_dist = extlookup('torque_install_dist')
+	$install_src = extlookup('torque_install_src')
 	$torque_admin = extlookup('torque_admin')
 	$compile_args_extra = extlookup('torque_complie_args_extra')
 	$torque_master = extlookup('torque_master_name')
 
-	$torque_initd = "${install_dist}/torque/contrib/init.d"
+	$torque_initd = "${install_src}/torque/contrib/init.d"
 	$complie_args = "--prefix=${install_dist} --enable-drmaa ${compile_args_extra}"
 
 }
