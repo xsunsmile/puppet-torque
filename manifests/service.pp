4da1d1b6595b96e69ff72ac0b49aafe344f6217f
Hao SUN <xsunsmile@gmail.com>
2011-05-25 17:38:06 +0900

    tiny fix

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,6 +3,12 @@ class torque::service {
 
 	include torque::params
 
+	replace { 'turn_on_verbose':
+		file => '/etc/default/rcS',
+		pattern => '^VERBOSE.*$',
+		replacement => 'VERBOSE=yes',
+	}
+
 	if $hostname == $torque::params::torque_master {
 		include torque::service_server
 		include torque::service_sched
