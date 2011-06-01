5b2622cd6c6e224f9783994e7e824b71f87664a5
Hao SUN <xsunsmile@gmail.com>
2011-06-01 19:24:34 +0900

    tiny fix

--- manifests/service_mom_node.pp
+++ manifests/service_mom_node.pp
@@ -3,8 +3,15 @@ class torque::service_mom_node inherits torque::service_mom {
 
 	include torque::pkg_install
 
+	file { "${torque::params::install_dist}/sbin/pbs_mom":
+		ensure => absent,
+	}
+
 	File["${torque::params::spool_dir}/mom_priv/config"] {
-		require +> Exec['install-torque-package'],
+		require +> [
+			Exec['install-torque-package'],
+			File["${torque::params::install_dist}/sbin/pbs_mom"],
+		],
 	}
 
 }
